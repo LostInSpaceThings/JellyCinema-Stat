694a5be2f3bacbdac474bc723d6e127025b7c6b1
Justin <jdawglosurdo@gmail.com>
2026-02-22 13:20:22 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$jellyfin_url = "YOUR JELLLYFIN URL HERE - EX http://10.38.1.118:8096"; 
+$api_key      = "your API KEY";
+$user_id      = "your USER ID";
+date_default_timezone_set('America/New_York');
+?>
